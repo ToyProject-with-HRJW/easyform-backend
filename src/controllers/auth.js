3bedc75385c0442cf76eab74a9e1141a8d9d8fe9
zwoong <zwoong6867@gmail.com>
2023-03-20 21:22:30 +0900

    Revert "Revert "Feat : app.js setting with token api""
    This reverts commit e1f4c466199c6ca7d02eda0c6cd953876108cdd4.

---
+++ src/controllers/auth.js
@@ -0,0 +1,5 @@
+async function getToken(req, res) {
+    console.log("controller init!")
+}
+
+export { getToken };
---
+++ src/routes/auth.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { validateToken } from "../middlewares/auth.js";
+import { getToken } from "../controllers/auth.js";
+
+const router = express.Router();
+router.post("/token", validateToken, getToken);
+
+export default router;
