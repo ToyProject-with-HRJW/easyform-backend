109f7284ad3ad4ef15b1da3f96ed022bf2d2c0cd
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2023-04-30 18:52:12 +0900

    Change : port number (#22)

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(4000, function () {
-  console.log("server has started on port 4000");
+app.listen(3000, function () {
+  console.log("server has started on port 3000");
 });
