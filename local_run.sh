2871f3f9e5bd9b7b28b00d9e0ee9268e74aebb43
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2023-05-29 16:40:12 +0900

    Feat : Base Project (Nest.js) (#27)

--- local_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker-compose build && 
-docker-compose up -d &&
-yarn install && 
-yarn start:local 
--- src/utils/consts.js
+++
@@ -1,9 +0,0 @@
-export const MISSING_REQUIRED_QUERY_PRAMETER =
-  "missing required query parameter";
-export const PRAMETER_VALUE_NOT_SATISFIED = "parameter value not satisfied";
-export const SNS_AUTH_FAILED = "sns auth failed";
-export const SUCCESS = "success";
-export const ACCESS_TOKEN = "accessToken";
-export const REFRESH_TOKEN = "refreshToken";
-export const INVALID_TOKEN = "invalid token";
-export const REISSUANCE_ACCESS_TOKEN = "Reissuance of access token";
