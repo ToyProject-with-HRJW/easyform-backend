b95a0fe7c0bdd211eb18d8b94183ba0b815ed462
zwoong <zwoong6867@gmail.com>
2023-05-29 16:35:36 +0900

    Feat : Base Project (Nest.js)

--- src/routes/survey.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { checkToken } from "../utils/common.js";
-import { validateCreateSurvey } from "../middlewares/survey.js";
-import { createSurvey } from "../controllers/survey.js";
-
-const router = express.Router();
-router.post("/", checkToken, validateCreateSurvey, createSurvey);
-
-export default router;
--- src/utils/enums.js
+++
@@ -1,5 +0,0 @@
-export const snsEnum = {
-  GOOGLE: 1,
-  KAKAO: 2,
-  NAVER: 3,
-};
