cc88535e666a48aea9556788e4bd603b520d7db6
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2023-06-07 23:12:47 +0900

    Feat : test pr (#31)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// main
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.PORT || 3000;
