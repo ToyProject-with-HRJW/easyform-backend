15b4432f62badb756ccbf5a03b68d21b4abc9874
zwoong <zwoong6867@gmail.com>
2023-06-07 22:54:08 +0900

    Feat : nest.js config setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+  }), 
+}),],
   controllers: [AppController],
   providers: [AppService],
 })
