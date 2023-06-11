e624cf16d12568aa9fad1de4bba22917086b4d2f
zwoong <zwoong6867@gmail.com>
2023-06-11 14:35:21 +0900

    Feat : PR-Template test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// main
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.PORT || 3000;
