c8fee305094d25054dcf6cf1986f6e10b8d728d1
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2023-06-18 13:31:02 +0900

    Feat: typeORM (#36)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: process.env.NODE_ENV === 'local' ? '.env.local' : '.env.prod',
-    validationSchema: Joi.object({
-      NODE_ENV: Joi.string().valid('local', 'prod').required(),
-  }), 
-}),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'local' ? '.env.local' : '.env.prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('local', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'prod' ? false : true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
