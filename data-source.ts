3195055a525add4977b73a0459a631a10d63640f
zwoong <zwoong6867@gmail.com>
2023-06-18 15:14:46 +0900

    Change: dataSource migrations

--- data-source.ts
+++ data-source.ts
@@ -8,8 +8,8 @@ const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ["dist/src/entity/*.entity.js"],
-  migrations: ["dist/src/database/migration/*.Migratio.js"],
+  entities: ["dist/src/entity/*.entity.js"], // autoLoadEntities 옵션을 사용 안하면 entities: [] 옵션에 사용할 모든 엔티티를 넣어줘야함
+  migrations: ["dist/src/migrations/*.js"],
 });
 
 export default dataSource;
