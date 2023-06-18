6704eecebfa83c464c69ba72d153c74a138c14e6
zwoong <zwoong6867@gmail.com>
2023-06-18 15:02:36 +0900

    Refactor: dataSource

--- data-source.ts
+++ data-source.ts
@@ -1,17 +1,15 @@
 import { DataSource } from 'typeorm';
 import 'src/config/env';
 
-const devDataSource = new DataSource({
+const dataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: true,
-  logging: true,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: ["dist/src/entity/*.entity.js"],
+  migrations: ["dist/src/database/migration/*.Migratio.js"],
 });
 
-export default devDataSource;
+export default dataSource;
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,16 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import 'src/config/env';
+import dataSource from '../../data-source';
 
 export const typeormConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  autoLoadEntities: true,
-  synchronize: true,
-  logging: true,
-  entities: ["dist/src/entity/*.entity.js"],
+  ...dataSource.options,
+  autoLoadEntities: true, // true인 경우, 엔티티들이 자동으로 불러와짐
+  synchronize: process.env.NODE_ENV === "local" ? true : false, // 프로그램 시작할 때마다 DB스키마를 자동으로 재생성 하는지 여부
+  logging: true, // 쿼리문과 에러가 로깅됨
 };
 
